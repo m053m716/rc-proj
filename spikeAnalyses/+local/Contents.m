cea2caa2b0ec25f850e63a20e23b7e02b65d70e3
Max-Home-Tower <max.murphy11@gmail.com>
2020-08-24 16:56:53 -0500

    Remove +local from gitignore

---
+++ spikeAnalyses/+local/Contents.m
@@ -0,0 +1,5 @@
+% LOCAL Package for local parameter configurations (to avoid `git` conflicts by adding it to `.gitignore`)
+% MATLAB Version 9.7 (R2019b Update 5) 28-May-2020
+%
+% Files
+%   defaults - Subset of defaults that will be specific to local machine 
